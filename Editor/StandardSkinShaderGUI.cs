311c3da1a55fd2c9140bc031ec7f68139d439d43
Alexander Earley <axe6801@rit.edu>
2019-01-27 10:48:35 -0500

    Added single map for AO, Smoothness, Thickness to Standard Added GUI to Standard

---
+++ Editor/StandardSkinShaderGUI.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityEditor
+{
+    internal class StandardSkinShaderGUI : ShaderGUI
+    {
+
+        private static class Styles
+        {
+            public static GUIContent color = new GUIContent("Color");
+            public static GUIContent tex = new GUIContent("Albedo (RGB)");
+            public static GUIContent norm = new GUIContent("Normal Map");
+
+            public static GUIContent smooth = new GUIContent("Smoothness Map");
+            public static GUIContent ao = new GUIContent("Ambient Occlusion Map");
+            public static GUIContent aoStr = new GUIContent("Ambient Occlusion Strength");
+            public static GUIContent thickness = new GUIContent("Thickness Map");
+            public static GUIContent s_ao_sss = new GUIContent("Thickness(R), AO(G), Smooth(B)");
+            public static GUIContent singleMap = new GUIContent("Use Single S/AO/SSS Map");
+
+            public static GUIContent smoothRemapBlack = new GUIContent("Smoothness Remap Black");
+            public static GUIContent smoothRemapWhite = new GUIContent("Smoothness Remap White");
+
+            public static GUIContent detailNorm = new GUIContent("Detail Normal Map");
+            public static GUIContent detailNormIntensity = new GUIContent("Detail Normal Map Intensity");
+
+            public static GUIContent sssColor = new GUIContent("Subsurface Color");
+            public static GUIContent sssPower = new GUIContent("Translucency Power");
+            public static GUIContent sssAmb = new GUIContent("Translucency Ambient");
+            public static GUIContent sssDist = new GUIContent("Translucency Distortion");
+            public static GUIContent sssTex = new GUIContent("Thickness Map");
+            public static GUIContent sssRemapBlack = new GUIContent("Translucency Remap Black");
+            public static GUIContent sssRemapWhite = new GUIContent("Translucency Remap White");
+            public static GUIContent sssEdgeValue = new GUIContent("SSS Edge Value");
+            public static GUIContent sssEdgePower = new GUIContent("SSS Edge Power");
+        }
+
+
+
+        MaterialProperty color = null;
+        MaterialProperty tex = null;
+        MaterialProperty norm = null;
+
+        MaterialProperty smooth = null;
+        MaterialProperty ao = null;
+        MaterialProperty aoStr = null;
+        MaterialProperty thickness = null;
+        MaterialProperty s_ao_sss = null;
+        MaterialProperty singleMap = null;
+
+        MaterialProperty smoothRemapBlack = null;
+        MaterialProperty smoothRemapWhite = null;
+
+        MaterialProperty detailNorm = null;
+        MaterialProperty detailNormIntensity = null;
+
+        MaterialProperty sssColor = null;
+        MaterialProperty sssPower = null;
+        MaterialProperty sssAmb = null;
+        MaterialProperty sssDist = null;
+        MaterialProperty sssRemapBlack = null;
+        MaterialProperty sssRemapWhite = null;
+        MaterialProperty sssEdgeValue = null;
+        MaterialProperty sssEdgePower = null;
+
+
+
+
+
+        MaterialEditor m_MaterialEditor;
+
+
+        public void FindProperties(MaterialProperty[] props)
+        {
+            color = FindProperty("_Color", props);
+            tex = FindProperty("_MainTex", props);
+            norm = FindProperty("_NormalTex", props);
+
+            smooth = FindProperty("_SmoothnessTex", props);
+            ao = FindProperty("_AOTex", props);
+            aoStr = FindProperty("_AOStrength", props);
+            thickness = FindProperty("_SSSTex", props);
+            s_ao_sss = FindProperty("_S_AO_SSS_Tex", props);
+            singleMap = FindProperty("_SingleMap", props);
+
+            smoothRemapBlack = FindProperty("_SmoothnessRemapBlack", props);
+            smoothRemapWhite = FindProperty("_SmoothnessRemapWhite", props);
+
+            detailNorm = FindProperty("_DetailNormalTex", props);
+            detailNormIntensity = FindProperty("_DetailNormalMapIntensity", props);
+
+            sssColor = FindProperty("_SSSColor", props);
+            sssPower = FindProperty("_SSSPower", props);
+            sssAmb = FindProperty("_SSSAmb", props);
+            sssDist = FindProperty("_SSSDist", props);
+            sssRemapBlack = FindProperty("_SSSRemapBlack", props);
+            sssRemapWhite = FindProperty("_SSSRemapWhite", props);
+            sssEdgeValue = FindProperty("_SSSEdgeValue", props);
+            sssEdgePower = FindProperty("_SSSEdgePower", props);
+        }
+
+
+        public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] props)
+        {
+            FindProperties(props);
+            m_MaterialEditor = materialEditor;
+            Material material = materialEditor.target as Material;
+
+            ShaderPropertiesGUI(material);
+        }
+
+        public void ShaderPropertiesGUI(Material material)
+        {
+            EditorGUIUtility.labelWidth = 0f;
+
+            GUILayout.Label("Primary Textures", EditorStyles.boldLabel);
+            m_MaterialEditor.TexturePropertySingleLine(Styles.tex, tex, color);
+            m_MaterialEditor.TexturePropertySingleLine(Styles.norm, norm);
+            m_MaterialEditor.TexturePropertySingleLine(Styles.detailNorm, detailNorm, detailNormIntensity);
+
+            GUILayout.Label("Material Properties", EditorStyles.boldLabel);
+            m_MaterialEditor.ShaderProperty(singleMap, Styles.singleMap);
+            if (material.GetFloat("_SingleMap") == 1.0f)
+            {
+                m_MaterialEditor.TexturePropertySingleLine(Styles.s_ao_sss, s_ao_sss);
+            }
+            else
+            {
+                m_MaterialEditor.TexturePropertySingleLine(Styles.smooth, smooth);
+                m_MaterialEditor.TexturePropertySingleLine(Styles.ao, ao);
+                m_MaterialEditor.TexturePropertySingleLine(Styles.sssTex, thickness);
+            }
+            m_MaterialEditor.ShaderProperty(aoStr, Styles.aoStr);
+            m_MaterialEditor.ShaderProperty(smoothRemapBlack, Styles.smoothRemapBlack);
+            m_MaterialEditor.ShaderProperty(smoothRemapWhite, Styles.smoothRemapWhite);
+            m_MaterialEditor.ShaderProperty(sssRemapBlack, Styles.sssRemapBlack);
+            m_MaterialEditor.ShaderProperty(sssRemapWhite, Styles.sssRemapWhite);
+
+            GUILayout.Label("Subsurface Scattering", EditorStyles.boldLabel);
+            m_MaterialEditor.ShaderProperty(sssColor, Styles.sssColor);
+            m_MaterialEditor.ShaderProperty(sssPower, Styles.sssPower);
+            m_MaterialEditor.ShaderProperty(sssAmb, Styles.sssAmb);
+            m_MaterialEditor.ShaderProperty(sssDist, Styles.sssDist);
+            m_MaterialEditor.ShaderProperty(sssEdgeValue, Styles.sssEdgeValue);
+            m_MaterialEditor.ShaderProperty(sssEdgePower, Styles.sssEdgePower);
+        }
+
+    }
+
+}
--- SSSShader_Standard.shader
+++ SSSShader_Standard.shader
@@ -1,6 +1,6 @@
 Shader "Skin/Skin Standard Shader" {
 	Properties {
-		[Header(Main Textures)]
+		//[Header(Main Textures)]
 		_Color ("Color", Color) = (0.8,0.8,0.8,1)
 		_MainTex ("Albedo (RGB)", 2D) = "white" {}
 		_NormalTex ("Normal Map", 2D) = "bump" {}
@@ -11,14 +11,18 @@ Shader "Skin/Skin Standard Shader" {
         _SmoothnessRemapWhite ("Smoothness Remap White", Range(-5,5)) = 0.7
         
         _AOTex ("Ambient Occlusion", 2D) = "white" {}
+        _AOStrength ("Ambient Occlusion Strength", Range(0,1)) = 1
+        
+        [Toggle] _SingleMap ("Use S/AO/SSS Map", Float) = 0.0
+        _S_AO_SSS_Tex ("Thickness (R), Ambient Occlusion (G), Smoothness (B)", 2D) = "white" {}
 
-        [Space]
-        [Header(Details)]
+        //[Space]
+        //[Header(Details)]
         _DetailNormalTex ("Detail Normal Map", 2D) = "bump" {}
         _DetailNormalMapIntensity ("Detail Normal Map Intensity", Range(-2,2)) = 1
 
-		[Space]
-		[Header(Subsurface Scattering)]
+		//[Space]
+		//[Header(Subsurface Scattering)]
 		_SSSColor ("Subsurface Color", Color) = (0.95,0.10,0.06,1)
 		_SSSPower ("Translucency Power", Float) = 5
 		_SSSAmb ("Translucency Ambient", Float) = 0.05
@@ -37,6 +41,7 @@ Shader "Skin/Skin Standard Shader" {
         
         CGPROGRAM
         #pragma surface surf Standard fullforwardshadows
+        #pragma shader_feature _SINGLEMAP_ON
         #pragma target 3.0
 
         struct Input {
@@ -46,12 +51,17 @@ Shader "Skin/Skin Standard Shader" {
 
         sampler2D _MainTex;
         sampler2D _NormalTex;
+        #ifdef _SINGLEMAP_ON
+        sampler2D _S_AO_SSS_Tex;
+        #else
         sampler2D _SmoothnessTex;
         sampler2D _AOTex;
+        #endif
         sampler2D _DetailNormalTex;
         fixed4 _Color;
         //half _Smoothness;
         half _DetailNormalMapIntensity;
+        half _AOStrength;
         half _SmoothnessRemapBlack;
         half _SmoothnessRemapWhite;
         
@@ -67,10 +77,19 @@ Shader "Skin/Skin Standard Shader" {
             nD.y *= _DetailNormalMapIntensity;
             o.Normal = normalize(n + nD);
             
-            o.Smoothness = saturate(lerp(_SmoothnessRemapBlack, _SmoothnessRemapWhite, tex2D (_SmoothnessTex, IN.uv_MainTex).r));
+            #ifdef _SINGLEMAP_ON
+                fixed3 map = tex2D (_S_AO_SSS_Tex, IN.uv_MainTex);
+                o.Smoothness = saturate(lerp(_SmoothnessRemapBlack, _SmoothnessRemapWhite, map.b));
+                o.Occlusion = map.g * _AOStrength;
+            #else
+                o.Smoothness = saturate(lerp(_SmoothnessRemapBlack, _SmoothnessRemapWhite, tex2D (_SmoothnessTex, IN.uv_MainTex).b));
+                o.Occlusion = tex2D (_AOTex, IN.uv_MainTex).g * _AOStrength;
+            #endif
+            
+            
             //o.Albedo = o.Smoothness;
             o.Metallic = 0;
-            o.Occlusion = tex2D (_AOTex, IN.uv_MainTex);
+            //o.Occlusion = tex2D (_AOTex, IN.uv_MainTex).g;
 
         }
         ENDCG
@@ -84,6 +103,7 @@ Shader "Skin/Skin Standard Shader" {
         
         CGPROGRAM
         #pragma surface surf SSS fullforwardshadows
+        #pragma shader_feature _SINGLEMAP_ON
         #pragma target 3.0
         #include "Translucency.cginc"
 
@@ -94,7 +114,11 @@ Shader "Skin/Skin Standard Shader" {
         };
 
         sampler2D _NormalTex;
+        #ifdef _SINGLEMAP_ON
+        sampler2D _S_AO_SSS_Tex;
+        #else
         sampler2D _SSSTex;
+        #endif
         sampler2D _DetailNormalTex;
         half _SpecularValue;
         fixed4 _SSSColor;
@@ -126,7 +150,11 @@ Shader "Skin/Skin Standard Shader" {
         void surf (Input IN, inout SurfaceOutput o) {
             o.Albedo = 0;
             //o.Alpha = tex2D (_SSSTex, IN.uv_MainTex);
-            o.Alpha = saturate(lerp(_SSSRemapBlack, _SSSRemapWhite, tex2D (_SSSTex, IN.uv_MainTex).r));
+            #ifdef _SINGLEMAP_ON
+                o.Alpha = saturate(lerp(_SSSRemapBlack, _SSSRemapWhite, tex2D (_S_AO_SSS_Tex, IN.uv_MainTex).r));
+            #else
+                o.Alpha = saturate(lerp(_SSSRemapBlack, _SSSRemapWhite, tex2D (_SSSTex, IN.uv_MainTex).r));
+            #endif
 
             fixed3 n = UnpackNormal(tex2D(_NormalTex, IN.uv_MainTex));
             fixed3 nD = UnpackNormal(tex2D(_DetailNormalTex, IN.uv_DetailNormalTex));
@@ -137,4 +165,5 @@ Shader "Skin/Skin Standard Shader" {
         ENDCG
 	}
 	FallBack "Diffuse"
+    CustomEditor "StandardSkinShaderGUI"
 }
